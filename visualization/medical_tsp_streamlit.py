741820e95853a3c870dff2262b2930f661fc97a8
Gabriel <gabriel96.gsr@gmail.com>
2025-10-04 19:12:56 -0300

    Move streamlit medical tsp to visualization folder

--- visualization/medical_tsp_streamlit.py
+++ visualization/medical_tsp_streamlit.py
@@ -1,3 +1,7 @@
+import os, sys
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(CURRENT_DIR))
+
 import streamlit as st
 import plotly.graph_objects as go
 import time
